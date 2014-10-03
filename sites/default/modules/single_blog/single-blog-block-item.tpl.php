b756cb3d2c219d35590ecc6b0467deca4b12f882
Sam Bourgeois <sam.bourgeois05@hotmail.com>
2014-10-03 16:25:32 +0200

    Make your own modules - Theming a Module - pg 115

---
+++ sites/default/modules/single_blog/single-blog-block-item.tpl.php
@@ -0,0 +1,13 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * HTML for an item in the single blog's block listing
+ *
+ * Available variables:
+ * - $classes: String of classes that can be used to
+ *   style contextually through CSS. It can be manipulated
+ *   through the variable $classes_array from preprocess functions.
+ *   The default values can be one or more of the following:
+ */
